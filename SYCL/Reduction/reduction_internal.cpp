60c1b68e7189cda7fc713cf1b5b224730a16a7cf
Andrei Elovikov <andrei.elovikov@intel.com>
2022-11-22 14:37:51 -0800

    Add time output

--- SYCL/Reduction/reduction_internal.cpp
+++ SYCL/Reduction/reduction_internal.cpp
@@ -4,6 +4,7 @@
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
 
 #include <sycl/sycl.hpp>
+#include <time.h>
 using namespace sycl;
 
 template <int Dims> auto get_global_range(range<Dims> Range) { return Range; }
@@ -78,6 +79,10 @@ static void test(RangeTy Range) {
 
   auto N = get_global_range(Range).size();
   int Expected = InitToIdentity ? N : Init + N;
+
+  std::time_t end_time =
+      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::cout << std::ctime(&end_time);
 #if defined(__PRETTY_FUNCTION__)
   std::cout << __PRETTY_FUNCTION__;
 #elif defined(__FUNCSIG__)
@@ -111,6 +116,9 @@ void testAllStrategies(RangeTy Range) {
 }
 
 int main() {
+  std::time_t end_time =
+      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::cout << std::ctime(&end_time);
   auto TestRange = [](auto Range) {
     testAllStrategies<true, true>(Range);
     testAllStrategies<true, false>(Range);
